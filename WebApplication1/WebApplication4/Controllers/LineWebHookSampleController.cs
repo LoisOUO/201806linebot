957dc7a14a0cd71ac43b1ec46841e6523551150c
Yi An Lin <lois.kiro@gmail.com>
2018-06-19 18:44:01 +0800

    回覆內容

--- WebApplication1/WebApplication4/Controllers/LineWebHookSampleController.cs
+++ WebApplication1/WebApplication4/Controllers/LineWebHookSampleController.cs
@@ -34,6 +34,58 @@ namespace WebApplication4.Controllers
                 }
                 //response OK
                 return Ok();
+             /*   //----------------------------//
+                //設定ChannelAccessToken(或抓取Web.Config)
+                this.ChannelAccessToken = channelAccessToken;
+                //取得Line Event(範例，只取第一個)
+                var LineEvent = this.ReceivedMessage.events.FirstOrDefault();
+                //配合Line verify 
+                if (LineEvent.replyToken == "00000000000000000000000000000000") return Ok();
+                //回覆訊息
+                //var UrseName= this.PushMessage(LineEvent.source.userId,"XXX"); //找個人的userID 私訊
+                //去抓資料庫
+                //this.GetUserInfo();
+                isRock.LineBot.Bot bot = new isRock.LineBot.Bot(channelAccessToken);
+                string LineId = ReceivedMessage.events.FirstOrDefault().source.userId;
+                var userInfo = bot.GetUserInfo(LineId);
+                if (LineEvent.type == "message")
+                {
+                    //this.ReplyMessage(LineEvent.replyToken,"我可以回答任何問題~" + LineEvent.message.text+ userInfo);
+                    if (LineEvent.message.type == "text")//收到文字 
+                    {
+                        string str = userInfo.displayName;
+
+                        if (LineEvent.message.text.Contains("hello"))
+                            str += "~hello";
+
+                        if (LineEvent.message.text.Contains("畢業門檻"))
+                            str += "，系必修有60學分，選修要28學分，核心要記得各領域各一個，通識10學分";
+
+                        if (LineEvent.message.text.Contains("必修"))
+                            str += "，系上必修60學分，英文6學分";
+
+                        if (LineEvent.message.text.Contains("選課") || LineEvent.message.text.Contains("選修"))
+                            str += "，已經超過選課時間了";
+
+                        if (LineEvent.message.text.Contains("退選") || LineEvent.message.text.Contains("二退"))
+                            str += " ，今年的二退時間已過，還沒退就認真上完吧";
+
+
+                        if (str.Length <= userInfo.displayName.Length + 10)
+                        {
+                            str += "，你可以問選課的事情。";
+                        }
+                        this.ReplyMessage(LineEvent.replyToken, str + "~");
+                    }
+
+                    if (LineEvent.message.type == "sticker") //收到貼圖
+                        this.ReplyMessage(LineEvent.replyToken, 1, 2);
+
+
+                }
+                //response OK
+                return Ok();
+                //----------------------------//*/
             }
             catch (Exception ex)
             {
--- WebApplication1/WebApplication4/default.aspx.cs
+++ WebApplication1/WebApplication4/default.aspx.cs
@@ -12,6 +12,7 @@ namespace WebApplication4
     {
         const string channelAccessToken = "wViS2NyjED7KQ+v20R68QvBVbSggjsHpsYtVSH8DoL+3y2YWiPwNT/m1QpUzpi73/fd2MzGhSKjONcwJLyt0y6lNW/JuyZyPjNOt3GTdiflCxPh0UI1V3x5oGlh7uAuepnlhYsrG7FVObg3CHAU9bAdB04t89/1O/w1cDnyilFU=";
         const string AdminUserId = "U26dd87213fa1f3d11d0a0a4d57a54cb5";
+        //  string str = userInfo.displayName;
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -27,7 +28,66 @@ namespace WebApplication4
         protected void Button2_Click(object sender, EventArgs e)
         {
             var bot = new Bot(channelAccessToken);
-            bot.PushMessage(AdminUserId, 1,2);
+            bot.PushMessage(AdminUserId, 1, 2);
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            var actions = new List<isRock.LineBot.TemplateActionBase>();
+            actions.Add(new isRock.LineBot.MessageAction() { label = "標題 文字回復", text = "回復文字" });
+            actions.Add(new isRock.LineBot.UriAction() { label = "標題 開啟URI", uri = new Uri("http://www.google.com") });
+            actions.Add(new isRock.LineBot.PostbackAction() { label = "標題 發生postack", data = "abc=aaa&def=111" });
+            var bot = new Bot(channelAccessToken);
+            var ButtonTempalteMsg = new isRock.LineBot.ButtonsTemplate()
+            {
+                //altText
+                title = "選項",
+                text = "請選擇一個",
+                altText = "請在手機上檢視",
+                thumbnailImageUrl = new Uri("https://img.ifun01.com/images/2017/12/21/02/3252_FcKYsi_3PI5fK.jpeg!r800x0.jpg"),
+                actions = actions
+            };
+            bot.PushMessage(AdminUserId, ButtonTempalteMsg);
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            var bot = new Bot(channelAccessToken);
+            var actions = new List<isRock.LineBot.TemplateActionBase>();
+            actions.Add(new isRock.LineBot.MessageAction() { label = "Yes", text = "Yes" });
+            actions.Add(new isRock.LineBot.MessageAction() { label = "No", text = "No" });
+
+            var ConfirmTemplate = new isRock.LineBot.ConfirmTemplate()
+            {
+
+                text = "請選擇一個",
+                altText = "請在手機上檢視",
+                actions = actions
+            };
+            bot.PushMessage(AdminUserId, ConfirmTemplate);
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            var actions = new List<isRock.LineBot.TemplateActionBase>();
+            actions.Add(new isRock.LineBot.MessageAction() { label = "標題 文字回復", text = "回復文字" });
+            actions.Add(new isRock.LineBot.UriAction() { label = "標題 開啟URI", uri = new Uri("http://www.google.com") });
+            actions.Add(new isRock.LineBot.PostbackAction() { label = "標題 發生postack", data = "abc=aaa&def=111" });
+            var bot = new Bot(channelAccessToken);
+            var Column = new isRock.LineBot.Column()
+            {
+                //altText
+                title = "選項",
+                text = "請選擇一個",
+                //altText = "請在手機上檢視",
+                thumbnailImageUrl = new Uri("https://img.ifun01.com/images/2017/12/21/02/3252_FcKYsi_3PI5fK.jpeg!r800x0.jpg"),
+                actions = actions
+            };
+            var CarouselTemplate = new isRock.LineBot.CarouselTemplate();
+            CarouselTemplate.columns.Add(Column);
+            CarouselTemplate.columns.Add(Column);
+            CarouselTemplate.columns.Add(Column);
+            bot.PushMessage(AdminUserId, CarouselTemplate);
         }
     }
 }
